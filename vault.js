6578d761c08f6393eb9d0483646de581b53a2f69
Abigail Thomas <abigailthomas@abigails-air.mynetworksettings.com>
2025-01-16 20:04:54 -0500

    initial commit, secure the vault

---
+++ vault.js
@@ -0,0 +1,10 @@
+//step 1. introductory message
+alert("You have received this message because you have been chose to open an important vault. Here is the secret combination");
+//step 2. calculate each number for the code using arithmetic operations
+let num1 = 6 + 4; 
+let num2 = 80/2; 
+let num3 = 40 - 1;
+// message
+const message = `You are winner :grin: and here is
+your numbers for the secure vault ":money_with_wings:${finalnum1} ${finalnum2} ${finalnum3}!"`;
+alert(message);
